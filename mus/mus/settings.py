f8975b04d7c7ffcdd5048d01dc3a2a12bf0fd684
artyom <artyomevseev@gmail.com>
2024-04-13 17:04:02 +0300

    Add + install DRF

--- mus/mus/settings.py
+++ mus/mus/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'projects'
+    'projects.apps.ProjectsConfig'
+    'rest-framework'
 ]
 
 MIDDLEWARE = [
