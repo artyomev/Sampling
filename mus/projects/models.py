4595d9473412badd954ddba56d888f4550dccdbe
artyom <artyomevseev@gmail.com>
2024-04-13 16:28:29 +0300

    Create project model

--- mus/projects/models.py
+++ mus/projects/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=300, blank=False)
+
+    def __str__(self):
+        return self.title
+
