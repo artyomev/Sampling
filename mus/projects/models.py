8363b9889d9e799e8a545bc6aed6c01c5eaaccb5
artyom <artyomevseev@gmail.com>
2024-04-16 01:44:49 +0300

    Add project team model

--- mus/projects/models.py
+++ mus/projects/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from musauth.models import MusUser
+
 
 class Project(models.Model):
     title = models.CharField(max_length=300, blank=False)
@@ -9,4 +11,19 @@ class Project(models.Model):
 
 
 class ProjectTeam(models.Model):
-    pass
+    partner = models.ForeignKey(MusUser,
+                                on_delete=models.CASCADE,
+                                related_name='teams_as_partner'
+                                )
+    manager = models.ForeignKey(MusUser,
+                                on_delete=models.CASCADE,
+                                related_name='teams_as_manager'
+                                )
+    incharge = models.ForeignKey(MusUser,
+                                on_delete=models.CASCADE,
+                                related_name='teams_as_incharge'
+                                )
+    staff = models.ManyToManyField(MusUser)
+    project = models.OneToOneField(Project,
+                                    on_delete=models.CASCADE,
+                                    primary_key=True)
