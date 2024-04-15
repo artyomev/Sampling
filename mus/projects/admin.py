fc9f24edbce36e66d3f72d30bb8c2752fe1023b5
artyom <artyomevseev@gmail.com>
2024-04-16 01:59:42 +0300

    Register project team in admin

--- mus/projects/admin.py
+++ mus/projects/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from projects.models import Project
+from projects.models import Project, ProjectTeam
 
 # Register your models here.
 admin.site.register(Project)
+admin.site.register(ProjectTeam)
