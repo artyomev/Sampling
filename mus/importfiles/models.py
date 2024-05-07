398c2026a50f0481db5b4f0c597033ef65d3e9df
artyom <artyomevseev@gmail.com>
2024-05-08 00:49:32 +0300

    Finished Importing Files Code

--- mus/importfiles/models.py
+++ mus/importfiles/models.py
@@ -6,7 +6,7 @@ from projects.models import Project
 class InitialUploadedFile(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     initial_file = models.FileField()
-    file_name = models.CharField(max_lenght=200, default="")
+    file_name = models.CharField(max_length=200, default="")
     date_uploaded = models.DateTimeField(auto_now_add=True)
     date_processed = models.DateTimeField(null=True, blank=True)
     status = models.CharField(max_length=150, default='loaded')
