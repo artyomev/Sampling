abcdbc858abafd78de922a9efe5331642a3bd88d
artyom <artyomevseev@gmail.com>
2024-05-08 01:27:19 +0300

    Add by_user attribute to model + migration

--- mus/importfiles/models.py
+++ mus/importfiles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from musauth.models import MusUser
 from projects.models import Project
 
 
@@ -8,8 +9,10 @@ class InitialUploadedFile(models.Model):
     initial_file = models.FileField()
     file_name = models.CharField(max_length=200, default="")
     date_uploaded = models.DateTimeField(auto_now_add=True)
+    by_user = models.ForeignKey(MusUser, on_delete=models.DO_NOTHING)
     date_processed = models.DateTimeField(null=True, blank=True)
     status = models.CharField(max_length=150, default='loaded')
+
 
     def __str__(self):
         return self.file_name
