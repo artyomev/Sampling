ceb11bdff16ec4d95c9757cb8d6b8c5773878662
artyom <artyomevseev@gmail.com>
2024-05-13 22:38:49 +0300

    Fix validation

--- mus/importfiles/services/parsers.py
+++ mus/importfiles/services/parsers.py
@@ -1,20 +1,29 @@
+from importfiles.models import InitialUploadedFile
+from importfiles.storage import uploads_storage
+
 
 def get_extension(file_name: str) -> str:
     return file_name.split('.')[-1]
 
 
-def parse_txt_file(file):
-    with open(file) as f:
+def parse_txt_file(ext:str,file, id:int):
+    if ext == 'txt':
+        initial_file = InitialUploadedFile.objects.filter(id=id).first()
+        col_del = initial_file.txt_column_delimiter
+    else:
+        col_del = ","
+    with uploads_storage.open(file.name, 'r') as f:
         first_line = f.readline()
-    if len(first_line.split('.'))>2:
+
+    if len(first_line.split(col_del))>2:
         print('Format error')
 
 
 def post_save_parse_file(file, id:int):
     excel_extensions = ['xslx', 'xlsb', 'xls', 'xlsm']
     python_extensions = ['txt', 'csv']
-
-    if get_extension(file.name) in python_extensions:
-        parse_txt_file(file)
+    ext = get_extension(file.name)
+    if ext in python_extensions:
+        parse_txt_file(ext, file, id)
     else:
         print('This feature not created yet')
--- mus/importfiles/services/validators.py
+++ mus/importfiles/services/validators.py
@@ -16,8 +16,8 @@ def validate_extension(data):
 
 def validate_delimiter_presence(data):
     try:
-        if get_extension(data['file_name']) == 'txt':
-            if data['txt_column_delimiter'] == "":
+        if get_extension(data['initial_file'].name) == 'txt':
+            if data.get('txt_column_delimiter', "") == "":
                 raise serializers.ValidationError('You have to determine column delimiter for text file!')
     except Exception as e:
         raise serializers.ValidationError(e)
