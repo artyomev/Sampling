e0a13510468d5135e0a766d20f66dbe1b330d815
artyom <artyomevseev@gmail.com>
2024-05-22 03:51:04 +0300

    Edit views

--- mus/projects/views.py
+++ mus/projects/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import DetailView, ListView
 
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, status
 from rest_framework.decorators import action, api_view
 
 from rest_framework.viewsets import GenericViewSet
@@ -41,15 +41,22 @@ class ProjectTeamViewSet(GenericViewSet):
     @action(detail=True, methods=['get'])
     def get_team_detailed(self, request, pk=None):
         """Возвращает детализированный список команды по проекту"""
-        users = Project.objects.get(id=pk).users.all()
-        return Response(
-            {'project_id': pk,
-             'partner': MusUserSerializer(users.filter(projectteamrole__role='Partner').first()).data,
-             'manager': MusUserSerializer(users.filter(projectteamrole__role='Manager').first()).data,
-             'incharge': MusUserSerializer(users.filter(projectteamrole__role='Incharge').first()).data,
-             'staff': [MusUserSerializer(u).data for u in users.filter(projectteamrole__role='Staff')]
-             }
-        )
+
+        try:
+            users = Project.objects.get(id=pk).users.all()
+            return Response(
+                {'project_id': pk,
+                 'partner': MusUserSerializer(users.filter(projectteamrole__role='Partner').first()).data,
+                 'manager': MusUserSerializer(users.filter(projectteamrole__role='Manager').first()).data,
+                 'incharge': MusUserSerializer(users.filter(projectteamrole__role='Incharge').first()).data,
+                 'staff': [MusUserSerializer(u).data for u in users.filter(projectteamrole__role='Staff')]
+                 }
+
+            )
+        except Exception as e:
+            return Response(
+                repr(e),
+                status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserProjectViewSet(GenericViewSet):
@@ -62,23 +69,34 @@ class UserProjectViewSet(GenericViewSet):
     @swagger_auto_schema(responses=userprojects_response_schema_dict)
     @action(detail=True, methods=['get'])
     def get_user_projects(self, request, pk=None):
-        user = MusUser.objects.filter(id=pk).first()
-        return Response({'user_id': pk,
+
+        try:
+            user = MusUser.objects.filter(id=pk).first()
+            return Response({'user_id': pk,
                          'projects' : [ProjectsSerializer(p).data for p in user.project_set.all()]
                          })
+        except Exception as e:
+            return Response(
+                repr(e),
+                status=status.HTTP_400_BAD_REQUEST)
 
-class ProjectFilesViewSet(mixins.RetrieveModelMixin,
-                           GenericViewSet):
+class ProjectFilesViewSet(GenericViewSet):
     serializer_class = InitialFileSerializer
 
     def get_queryset(self):
         pass
     @action(detail=True, methods=['get'])
     def get_project_files(self, request, pk=None):
-        project = Project.objects.filter(id=pk).first()
-        return Response({'project_id': pk,
-                         'files': [InitialFileSerializer(p).data for p in project.initialuploadedfile_set.all()]
-                         })
+
+        try:
+            project = Project.objects.filter(id=pk).first()
+            return Response({'project_id': pk,
+                             'files': [InitialFileSerializer(p).data for p in project.initialuploadedfile_set.all()]
+                             })
+        except Exception as e:
+            return Response(
+                repr(e),
+                status=status.HTTP_400_BAD_REQUEST)
 
 class ProjectDetail(DetailView):
     model = Project
